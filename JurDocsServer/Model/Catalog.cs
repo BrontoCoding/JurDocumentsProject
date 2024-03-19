4b6ba9ba6d1aabb1fd798b6cf3277491c573cf73
SergeyXP <s.pusthotin@gmail.com>
2024-03-19 20:54:28 +0400

    1

--- JurDocsServer/Model/Catalog.cs
+++ JurDocsServer/Model/Catalog.cs
@@ -23,16 +23,16 @@
         /// <summary>
         /// Список ид пользователей, которые могут обновлять каталог (добавлять/изменять)
         /// </summary>
-        public List<int> Update { get; } = [];
+        public List<int> Update { get; set; } = [];
 
         /// <summary>
         /// Список ид пользователей, которые могут просматривать каталог 
         /// </summary>
-        public List<int> Read { get; } = [];
+        public List<int> Read { get; set; } = [];
 
         /// <summary>
         /// Список ид пользователей, которые могут удалять каталог
         /// </summary>
-        public List<int> Delete { get; } = [];
+        public List<int> Delete { get; set; } = [];
     }
 }
